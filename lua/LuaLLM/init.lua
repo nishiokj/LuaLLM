b9b37109ead3c8092f2eb60bb72c5a958343a9dd
nishiokj <68032300+nishiokj@users.noreply.github.com>
2025-02-13 17:19:11 -0600

    Update init.lua
    adding code to write and read from zig server, and writing at cursor

--- lua/LuaLLM/init.lua
+++ lua/LuaLLM/init.lua
@@ -30,6 +30,7 @@ function M.start_zig_server()
     on_exit = function(_, exit_code, _)
       vim.schedule(function() print("Zig server exited with code: " .. exit_code) end)
     end,
+      
   })
 
   if job_id <= 0 then
@@ -53,26 +54,105 @@ function M.connect_to_zig()
         return vim.schedule(function() vim.notify("Error reading from Zig server: " .. err, vim.log.levels.ERROR) end)
       end
       if chunk then
-        vim.schedule(function() print("Received from Zig server: " .. chunk) end)
+        vim.schedule(function() M.write_string_at_cursor(chunk) end)
+      
       else
         vim.schedule(function() vim.notify("Zig server closed the connection", vim.log.levels.WARN) end)
         client:close()
       end
     end)
+      if callback then
+      callback()
+    end
   end)
 end
 
-function M.send_to_zig(input)
-  if not client then
-    return vim.notify("No TCP client available to send data", vim.log.levels.ERROR)
-  end
-  client:write(input, function(err)
-    if err then
-      vim.schedule(function() vim.notify("Error sending data: " .. err, vim.log.levels.ERROR) end)
+function M.get_lines_until_cursor()
+  local current_buffer = vim.api.nvim_get_current_buf()
+  local current_window = vim.api.nvim_get_current_win()
+  local cursor_position = vim.api.nvim_win_get_cursor(current_window)
+  local row = cursor_position[1]
+
+  local lines = vim.api.nvim_buf_get_lines(current_buffer, 0, row, true)
+
+  return table.concat(lines, '\n')
+end
+
+function M.get_visual_selection()
+  local _, srow, scol = unpack(vim.fn.getpos 'v')
+  local _, erow, ecol = unpack(vim.fn.getpos '.')
+
+  if vim.fn.mode() == 'V' then
+    if srow > erow then
+      return vim.api.nvim_buf_get_lines(0, erow - 1, srow, true)
     else
-      vim.schedule(function() print("Data sent to Zig server") end)
+      return vim.api.nvim_buf_get_lines(0, srow - 1, erow, true)
     end
+  end
+
+  if vim.fn.mode() == 'v' then
+    if srow < erow or (srow == erow and scol <= ecol) then
+      return vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
+    else
+      return vim.api.nvim_buf_get_text(0, erow - 1, ecol - 1, srow - 1, scol, {})
+    end
+  end
+
+  if vim.fn.mode() == '\22' then
+    local lines = {}
+    if srow > erow then
+      srow, erow = erow, srow
+    end
+    if scol > ecol then
+      scol, ecol = ecol, scol
+    end
+    for i = srow, erow do
+      table.insert(lines, vim.api.nvim_buf_get_text(0, i - 1, math.min(scol - 1, ecol), i - 1, math.max(scol - 1, ecol), {})[1])
+    end
+    return lines
+  end
+end
+
+function M.write_string_at_cursor(str)
+  vim.schedule(function()
+    local current_window = vim.api.nvim_get_current_win()
+    local cursor_position = vim.api.nvim_win_get_cursor(current_window)
+    local row, col = cursor_position[1], cursor_position[2]
+
+    local lines = vim.split(str, '\n')
+
+    vim.cmd("undojoin")
+    vim.api.nvim_put(lines, 'c', true, true)
+
+    local num_lines = #lines
+    local last_line_length = #lines[num_lines]
+    vim.api.nvim_win_set_cursor(current_window, { row + num_lines - 1, col + last_line_length })
   end)
+end
+
+function M.trigger_zig()
+  -- Gather input from the screen.
+  -- You might adjust this to get a visual selection or other region.
+  local input = M.get_lines_until_cursor()
+
+  local function send_input()
+    uv.write(client, input, function(err)
+      if err then
+        vim.schedule(function()
+          vim.notify("Error sending data to Zig server: " .. err, vim.log.levels.ERROR)
+        end)
+      else
+        vim.schedule(function() print("Input sent to Zig server") end)
+      end
+    end)
+  end
+
+  -- If the client is not yet connected, connect and then send.
+  if not client:is_readable() then
+    connect_client(send_input)
+  else
+    send_input()
+  end
 end
 
 return M
